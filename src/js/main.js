cc22833e2127574de63a25ebd475e5c2e6a03a64
Florian Bouvot <florian.bouvot@gmail.com>
2017-05-10 14:46:59 +0200

    Rename nav id

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,8 @@ svg4everybody();
  * Toggle on/off nav on click
  */
 
-var navBtn = document.getElementById('js-nav-toggle-btn');
-var navMenu = document.getElementById('js-nav-toggle-menu');
+var navBtn = document.getElementById('js-nav-btn');
+var navMenu = document.getElementById('js-nav-menu');
 
 if (navBtn && navMenu) {
   navBtn.onclick = function toggleMenu() {
